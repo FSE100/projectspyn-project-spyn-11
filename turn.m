fff27623b3db771ccca1d05ec162cb6aa53aee20
Raj Datta <raj@datta.me>
2019-12-06 16:41:36 -0700

    Delete turn.m

--- turn.m
+++
@@ -1,8 +0,0 @@
-function turn(brick, time, direction) % for direction, 1 = right, -1 = left
-    brick.MoveMotor('A', direction*-20);
-    brick.MoveMotor('D', direction*19.4);
-    pause(time);
-    brick.StopMotor('AD', 'Brake');
-    
-    %calibrate(brick);
-end
