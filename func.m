2d2848e7fc4929a28da16ce231e42d30c77c00eb
Raj Datta <raj@datta.me>
2019-12-06 16:41:06 -0700

    Delete func.m

--- func.m
+++
@@ -1,6 +0,0 @@
-function outputArg1 = func(inputArg1,inputArg2)
-    %FUNCTION Summary of this function goes here
-    %   Detailed explanation goes here
-    outputArg1 = std([inputArg1,inputArg2])
-    end
-
