8dda9018d4cf2cd8ff9a3524dcfdaff7145dfee7
Raj Datta <raj@datta.me>
2019-12-06 16:40:47 -0700

    Delete calibrateRam.m

--- calibrateRam.m
+++
@@ -1,8 +0,0 @@
-function [outputArg1,outputArg2] = calibrateRam(inputArg1,inputArg2)
-%CALIBRATERAM Summary of this function goes here
-%   Detailed explanation goes here
-    moveForward(brick, 15, 0.1, -1)
-    if(brick.TouchPressed(3))
-        
-    end
-end
